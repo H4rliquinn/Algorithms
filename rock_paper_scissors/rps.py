fc00f5cfaefa1da0f85acefbe7df67e205edd902
Jason Sonnichsen <Harliquinn@gmail.com>
2020-01-22 18:35:13 -0500

    RPS Challenge Complete

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,23 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    if n == 0:
+        return [[]]
+    elif n == 1:
+        return [['rock'], ['paper'], ['scissors']]
+    else:
+        prev = rock_paper_scissors(n-1)
+        ret_list = []
+        for x in prev:
+            ret_list += [x+['rock'], x+['paper'], x+['scissors']]
+        return ret_list
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
