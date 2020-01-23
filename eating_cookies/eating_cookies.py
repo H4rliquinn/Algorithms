77efed88c37aa2f7a4b48c72fc033fd62c0b57d1
Jason Sonnichsen <Harliquinn@gmail.com>
2020-01-23 15:39:48 -0500

    Eating Cookie complete and tested

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -79,22 +79,31 @@ import math
 #         perms += 1
 
 #     return perms
+cookie_cache = [0, 1]
 
-def eating_cookies(n, cache=None):
+
+def eating_cookies(n, cache={}):
+    if type(cache) is list:
+        cache = {}
     if n == 0:
         return 1
-    elif n <= 0:
+    elif n < 0:
         return 0
     elif n == 1:
         return 1
     elif n > 1:
-        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+        if cache.get(n, 0):
+            return cache[n]
+        new = eating_cookies(n-1, cache)
+        if not cache.get(n-1, 0):
+            cache[n-1] = new
+        return new + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         num_cookies = int(sys.argv[1])
         print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
-            ways=eating_cookies(num_cookies), n=num_cookies))
+            ways=eating_cookies(num_cookies, cookie_cache), n=num_cookies))
     else:
         print('Usage: eating_cookies.py [num_cookies]')
